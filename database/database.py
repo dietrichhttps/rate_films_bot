03e00d5ef9ef6fc497fff2e0df05998d2b024fd8
Alice Glass <abayhanov1998@gmail.com>
2024-03-23 00:45:40 +0300

    Написал движок базы данных

---
+++ database/database.py
@@ -0,0 +1,14 @@
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from sqlalchemy import create_engine
+from database.db_config import settings
+
+sync_engine = create_engine(
+    url=settings.DATABASE_URL_psycopg,
+    echo=False
+)
+
+session_factory = sessionmaker(sync_engine)
+
+
+class Base(DeclarativeBase):
+    pass
