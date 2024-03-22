0033a4f1fae40a68b30447e7c46bf0251812ee48
Alice Glass <abayhanov1998@gmail.com>
2024-03-23 00:46:40 +0300

    Написал модели бд

---
+++ database/models.py
@@ -0,0 +1,34 @@
+from sqlalchemy import Column, Integer, ForeignKey, Table, Text
+from sqlalchemy.orm import relationship
+
+from database.database import Base
+
+# Создаем таблицу для связи между фильмами и оценками
+film_rating_association = Table(
+    'film_rating_association', Base.metadata,
+    Column('film_id', Integer, ForeignKey('films.id')),
+    Column('rating_id', Integer, ForeignKey('ratings.id')),
+)
+
+
+class Film(Base):
+    __tablename__ = 'films'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(Text)
+    wiki_link = Column(Text)
+
+    # Определяем отношение "много ко многим" к оценкам
+    ratings = relationship('Rate', secondary=film_rating_association,
+                           back_populates='films')
+
+
+class Rate(Base):
+    __tablename__ = 'ratings'
+
+    id = Column(Integer, primary_key=True)
+    rating = Column(Integer)
+
+    # Определяем отношение "много ко многим" к книгам
+    films = relationship('Film', secondary=film_rating_association,
+                         back_populates='ratings')
