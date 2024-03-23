27ca9da2a620d988fa6224530d4caf575b830c79
Alice Glass <abayhanov1998@gmail.com>
2024-03-23 19:52:37 +0300

    Изменил message на callback в фильтре IsRate

--- filters/filters.py
+++ filters/filters.py
@@ -1,10 +1,10 @@
 import re
 
 from aiogram.filters import BaseFilter
-from aiogram.types import Message
+from aiogram.types import CallbackQuery
 
 
 class IsRate(BaseFilter):
-    async def __call__(self, message: Message):
-        rate = message.text.strip()
+    async def __call__(self, callback: CallbackQuery):
+        rate = callback.data.split('rate-')[1].strip()
         return re.match(r'^(?:[0-9]|10)$', rate) and rate.isdigit()
