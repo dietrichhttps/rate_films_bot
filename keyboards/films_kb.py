2d9677014179db077183fb86a849748d90630e3e
Alice Glass <abayhanov1998@gmail.com>
2024-03-23 22:34:40 +0300

    Добавил новые генераторы клавиатур

--- keyboards/films_kb.py
+++ keyboards/films_kb.py
@@ -15,3 +15,56 @@ def create_suggestions_keyboard(
             url=link
         ))
     return kb_builder.as_markup()
+
+
+def create_my_films_keyboard() -> InlineKeyboardMarkup:
+    kb_builder = InlineKeyboardBuilder()
+    all_films_btn = InlineKeyboardButton(
+        text='Все фильмы',
+        callback_data='all_films'
+    )
+    films_by_rate_btn = InlineKeyboardButton(
+        text='Фильмы по оценкам',
+        callback_data='films_by_rate'
+    )
+    search_my_films_btn = InlineKeyboardButton(
+        text='Поиск фильмов 🔎',
+        callback_data='search_my_films'
+    )
+    kb_builder.row(
+        all_films_btn, films_by_rate_btn,
+        width=2
+    )
+    kb_builder.row(search_my_films_btn)
+    return kb_builder.as_markup()
+
+
+def create_all_my_films_keyboard(
+        films: dict[int, str]) -> InlineKeyboardMarkup:
+    kb_builder = InlineKeyboardBuilder()
+
+    for film_id, title in films.items():
+        kb_builder.row(InlineKeyboardButton(
+            text=title,
+            callback_data=f'my_film-{film_id}'
+        ))
+    return kb_builder.as_markup()
+
+
+def create_film_info_keyboard() -> InlineKeyboardMarkup:
+    kb_builder = InlineKeyboardBuilder()
+    rate_btn = InlineKeyboardButton(
+        text='Оценка',
+        callback_data='my_film_rate'
+    )
+    review_btn = InlineKeyboardButton(
+        text='Рецензия',
+        callback_data='my_film_review'
+    )
+    wiki_link_btn = InlineKeyboardButton(
+        text='Страница в Википедии',
+        callback_data='my_film_rate'
+    )
+    kb_builder.row(rate_btn, review_btn, width=2)
+    kb_builder.row(wiki_link_btn)
+    return kb_builder.as_markup()
