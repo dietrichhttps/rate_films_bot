eacbf0f5ea927fa1faf7bf234d397ce956d11064
Alice Glass <abayhanov1998@gmail.com>
2024-03-24 17:46:34 +0300

    rating вместо rate для однозначности кода

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -8,15 +8,15 @@ from aiogram.fsm.state import default_state, State, StatesGroup
 from aiogram.fsm.storage.memory import MemoryStorage
 
 from lexicon.lexicon import LEXICON
-from filters.filters import IsRate
+from filters.filters import IsRating
 from services.film_service import search_films
 from keyboards.films_kb import (create_suggestions_keyboard,
                                 create_film_info_keyboard,
                                 create_my_films_keyboard,
                                 create_all_my_films_keyboard)
-from keyboards.rates_kb import create_rates_keyboard
+from keyboards.ratings_kb import create_ratings_keyboard
 
-from database.orm import FilmORM, RateORM
+from database.orm import FilmORM, RatingORM
 
 router = Router()
 storage = MemoryStorage()
@@ -25,7 +25,7 @@ logger = logging.getLogger(__name__)
 
 class FSMRateFilm(StatesGroup):
     send_title = State()
-    send_rate = State()
+    send_rating = State()
 
 
 class FSMReviewFilm(StatesGroup):
@@ -35,7 +35,19 @@ class FSMReviewFilm(StatesGroup):
 
 class FSMMyFilms(StatesGroup):
     my_films = State()
-    my_rates = State()
+    my_ratings = State()
+
+
+class StartCommandHandler:
+    pass
+
+
+class RateFilmCommandHandler:
+    pass
+
+
+class MyFilmsCommandHandler:
+    pass
 
 
 # Этот хэндлер будет срабатывать на команду "/start" -
@@ -56,7 +68,6 @@ async def process_rate_film_command(message: Message, state: FSMContext):
 # Этот хэндлер будет срабатывать на текст с названием фильма
 @router.message(StateFilter(FSMRateFilm.send_title))
 async def process_film_title_sent(message: Message, state: FSMContext):
-    logger.debug('Inside process_film_title_sent')
     query_title = message.text
     suggestions = search_films(query_title)
 
@@ -82,35 +93,35 @@ async def process_suggestion_press(callback: CallbackQuery, state: FSMContext):
 
     await callback.message.edit_text(
         text='Отправьте оценку',
-        reply_markup=create_rates_keyboard()
+        reply_markup=create_ratings_keyboard()
     )
     await callback.answer()
     await state.set_state(FSMRateFilm.send_rate)
 
 
 # Этот хэндлер будет срабатывать на оценку фильма
-@router.callback_query(StateFilter(FSMRateFilm.send_rate), IsRate())
-async def process_film_rate_sent(callback: CallbackQuery, state: FSMContext):
-    rate = int(callback.data.split('rate-')[1])
-    await state.update_data(rate=rate)
+@router.callback_query(StateFilter(FSMRateFilm.send_rate), IsRating())
+async def process_film_rating_sent(callback: CallbackQuery, state: FSMContext):
+    rating = int(callback.data.split('rating-')[1])
+    await state.update_data(rating=rating)
 
     await callback.answer('Оценка принята!')
     await callback.answer()
 
     film_data = await state.get_data()
     film_data.pop('suggestions', None)
-    title, wiki_link, rate = film_data.values()
+    title, wiki_link, rating = film_data.values()
 
     FilmORM.set_film(title, wiki_link)
     film_id = FilmORM.get_film_id(wiki_link)
-    RateORM.set_rating(film_id, rate)
+    RatingORM.set_rating(film_id, rating)
     await state.clear()
 
 
 # Этот хэндлер будет срабатывать, если во время отправки
 # оценки будет введенено что-то не то
-@router.callback_query(StateFilter(FSMRateFilm.send_rate))
-async def warning_not_rate(callbback: CallbackQuery):
+@router.callback_query(StateFilter(FSMRateFilm.send_rating))
+async def warning_not_rating(callbback: CallbackQuery):
     await callbback.answer(
         text='Оценка должна быть по 10 балльной шкале\n\n'
              'Попробуйте еще раз!')
@@ -157,6 +168,6 @@ async def process_my_film_press(callback: CallbackQuery):
 # Этот хэндлер будет срабатывать на нажатие кнопки "Оценка" в клавиатуре
 # create_film_info_keyboard
 @router.callback_query(StateFilter(FSMMyFilms.my_films),
-                       F.data == 'my_film_rate')
-async def process_my_films_rate_press(callback: CallbackQuery):
+                       F.data == 'my_film_rating')
+async def process_my_films_rating_press(callback: CallbackQuery):
     pass
