940ce7676322d2ac68650edf245e39a43794949b
Alice Glass <abayhanov1998@gmail.com>
2024-03-24 17:34:26 +0300

    rating вместо rate для однозначности кода

--- database/models.py
+++ database/models.py
@@ -4,10 +4,10 @@ from sqlalchemy.orm import relationship
 from database.database import Base
 
 # Создаем таблицу для связи между фильмами и оценками
-film_rate_association = Table(
-    'film_rate_association', Base.metadata,
+film_rating_association = Table(
+    'film_rating_association', Base.metadata,
     Column('film_id', Integer, ForeignKey('films.id')),
-    Column('rate_id', Integer, ForeignKey('rates.id')),
+    Column('rating_id', Integer, ForeignKey('ratings.id')),
 )
 
 
@@ -19,16 +19,16 @@ class Film(Base):
     wiki_link = Column(Text)
 
     # Определяем отношение "много ко многим" к оценкам
-    rates = relationship('Rate', secondary=film_rate_association,
+    rates = relationship('Rate', secondary=film_rating_association,
                          back_populates='films')
 
 
 class Rate(Base):
-    __tablename__ = 'rates'
+    __tablename__ = 'ratings'
 
     id = Column(Integer, primary_key=True)
-    rate = Column(Integer)
+    rating = Column(Integer)
 
     # Определяем отношение "много ко многим" к книгам
-    films = relationship('Film', secondary=film_rate_association,
-                         back_populates='rates')
+    films = relationship('Film', secondary=film_rating_association,
+                         back_populates='ratings')
