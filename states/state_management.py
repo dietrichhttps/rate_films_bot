4ec99a005f3fb43406184e4ce2e0bccb140b64e8
Alice Glass <abayhanov1998@gmail.com>
2024-03-25 20:00:56 +0300

    Вынес работу с состояниями в отдельный модуль

---
+++ states/state_management.py
@@ -0,0 +1,32 @@
+# Класс для узла списка состояний
+class StateNode:
+    def __init__(self, state):
+        self.state = state
+        self.prev_node = None
+
+
+# Класс для односвязного списка состояний
+class StateLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def add_state(self, state):
+        new_node = StateNode(state)
+        new_node.prev_node = self.head
+        self.head = new_node
+
+    def get_current_state(self):
+        if self.head:
+            return self.head.state
+        else:
+            return None
+
+    def go_back(self):
+        if self.head:
+            self.head = self.head.prev_node
+
+    def get_current_state_str(self) -> str:
+        current_state = self.get_current_state()
+        if current_state:
+            current_state_str = str(current_state)[8:-2]
+            return current_state_str
