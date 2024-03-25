e828d2624a189be917cc3c29ea6f1c5ea8d10aeb
Alice Glass <abayhanov1998@gmail.com>
2024-03-25 19:47:22 +0300

    Изменил фильтр IsRating

--- filters/filters.py
+++ filters/filters.py
@@ -6,5 +6,8 @@ from aiogram.types import CallbackQuery
 
 class IsRating(BaseFilter):
     async def __call__(self, callback: CallbackQuery):
-        rating = callback.data.split('rating-')[1].strip()
-        return re.match(r'^(?:[0-9]|10)$', rating) and rating.isdigit()
+        if callback.data.startswith('rating-'):
+            rating = callback.data.split('rating-')[1].strip()
+            return re.match(r'^(?:[0-9]|10)$', rating) and rating.isdigit()
+        return False
+    
