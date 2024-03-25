2f02d23670ba07d950dc1e8a83a35a76b9756e6f
Alice Glass <abayhanov1998@gmail.com>
2024-03-25 20:01:13 +0300

    Вынес состояния в отдельный модуль

---
+++ states/states.py
@@ -0,0 +1,29 @@
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.storage.memory import MemoryStorage
+
+storage = MemoryStorage()
+
+
+# Группа состояний для команды /main_menu
+class FSMMainMenu(StatesGroup):
+    main_menu = State()
+
+
+# Группа состояний для команды /rate_film
+class FSMRateFilmMenu(StatesGroup):
+    send_title = State()
+    select_suggestion = State()
+    send_rating = State()
+    rate_complete = State()
+
+
+# Группа состояний для команды /review_film
+class FSMReviewFilmMenu(StatesGroup):
+    send_title = State()
+    send_review = State()
+
+
+# Группа состояний для команды /my_films
+class FSMMyFilmsMenu(StatesGroup):
+    my_films = State()
+    my_ratings = State()
